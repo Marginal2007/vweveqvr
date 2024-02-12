3ede2cf28bf020089a063144550b98c01d392abe
Marginal2007 <osipow.sergey2007@yandex.ru>
2024-02-12 18:30:51 +0300

    4

--- main.py
+++ main.py
@@ -12,13 +12,20 @@ def index():
 def index1():
     return 'И на Марсе будут яблони цвести!'
 
+
 @app.route('/promotion')
 def index2():
     return render_template('index.html')
 
+
 @app.route('/image_mars')
 def mars():
     return render_template('index1.html')
+
+
+@app.route('/promotion_image')
+def promotion():
+    return render_template('index2.html')
 
 
 if __name__ == '__main__':
